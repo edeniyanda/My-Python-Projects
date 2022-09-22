e6ceeee35b1cc58295c9c2d99e7b49c8d3eb7e25
Eden Iyanda <edeniyanda@gmail.com>
2022-09-22 07:25:09 +0100

    Upgraded

--- guess.py
+++ guess.py
@@ -5,11 +5,27 @@ guess_word = "Eden"
 guess_count = 1
 guess_limit = 3
 trial_off = False
-word = input("Enter my name")
-while word != guess_word and not(trial_off):
+trial = 3
+s = "s"
+while True:
+    word = input("Enter my name")
+    try:
+        float(word)
+        print("Invalid input, Please enter a Alphabetical data")
+        continue
+    except:
+        fw = str(word)
+        break
+
+while fw != guess_word and not(trial_off):
     if guess_count < guess_limit:
-        word = input("Try again, you are almost there!")
         guess_count += 1
+        trial -= 1
+        if trial == 1:
+            s =""
+            fw = input(f"You have {trial} trial{s} left, try again!!")
+        else:
+            fw = input(f"You have {trial} trial{s} left, try again!!")
     else:
         trial_off = True
 
